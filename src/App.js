21ab5decd0b052ee535e57ec6919ada83e091b51
Wednesday <yensonyu@gmail.com>
2021-03-07 15:17:11 -0800

    create picture component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper"
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import { HashRouter, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter basename='/'>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </HashRouter>
   );
 }
 
---
+++ src/components/Picture/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Picture() {
+    return (
+        <div className="col-lg-5 mt-5 pl-5">
+            <img class="pic" alt="Yuwen Yu" src="./assets/images/mypic2.JPG" />
+        </div>
+    );
+}
+
+export default Picture;
