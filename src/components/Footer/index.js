136cd6e6165de687d019e96f8a55e4444e7e9bb6
Wednesday <yensonyu@gmail.com>
2021-03-06 22:24:35 -0800

    create footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return (
+        <footer className="footer bg-light">
+            <p>&copy; Yuwen Yu all rights reserved 2020</p>
+        </footer>
+    );
+}
+
+export default Footer;
