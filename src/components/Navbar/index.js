8ac23f40713fa883ccd08d8ad39dabdea227f1ba
Wednesday <yensonyu@gmail.com>
2021-03-06 22:48:08 -0800

    create navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+function Navbar() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <Link className="navbar-brand font-weight-bold" to="/">
+          Yuwen Yu
+        </Link>
+        <div className="navbar-collapse justify-content-end">
+          <ul className="navbar-nav">
+            <li class="nav-item active">
+              <Link className="nav-link" to="/">
+                About
+              </Link>
+            </li>
+            <li className="nav-item active">
+              <Link className="nav-link" to="/projects">
+                Work
+              </Link>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link" href="./assets/resume/Resume-YuwenY.pdf" download>Resume</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+}
+
+export default Navbar;
